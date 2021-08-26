b7519f0a80cc8c8a0d3dbd24c33924dcff441eb6
keithawess <61606760+keithwess@users.noreply.github.com>
2021-08-26 16:43:41 -0500

    Adds Navlink and Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import './App.css';
+import {
+  BrowserRouter as Router,
+  NavLink,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import Projects from "./components/Projects";
+import About from "./components/About";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <nav>
+        <NavLink activeClassName="active" className="" to="/projects">
+          Projects
+        </NavLink>
+        <NavLink activeClassName="active" className="" to="/about">
+          About
+        </NavLink>
+      </nav>
+
+      <main>
+        <Switch>
+          <Route path="/projects">
+            <Projects />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </main>
+
+    </Router>
   );
 }
 
