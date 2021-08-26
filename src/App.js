8613a9e076d089f6d4ea3dca67f5f2e138b5c822
keithawess <61606760+keithwess@users.noreply.github.com>
2021-08-26 17:37:46 -0500

    Adds placeholder for logo, name, and styling

--- src/App.js
+++ src/App.js
@@ -5,18 +5,32 @@ import {
   Route,
   Redirect,
 } from "react-router-dom";
-import "./App.css"
+import "./App.css";
 import Projects from "./components/Projects";
 import About from "./components/About";
 
 function App() {
   return (
     <Router>
+      <header className="flex bg-black text-white">
+        <div className="circle border border-white logo margin-right">&nbsp;</div>
+        <div className="header-name">Keith Wess</div>
+        
+      </header>
+
       <nav className="flex justify-space-evenly width-full bg-blue">
-        <NavLink activeClassName="active" className="flex-half border-white border-right text-center text-plain text-white" to="/projects">
+        <NavLink
+          activeClassName="active"
+          className="flex-half border-white border-right text-center text-plain text-white"
+          to="/projects"
+        >
           Projects
         </NavLink>
-        <NavLink activeClassName="active" className="flex-half border-white text-center text-plain text-white" to="/about">
+        <NavLink
+          activeClassName="active"
+          className="flex-half border-white text-center text-plain text-white"
+          to="/about"
+        >
           About
         </NavLink>
       </nav>
@@ -30,11 +44,10 @@ function App() {
             <About />
           </Route>
           <Route path="*">
-            <Redirect to="/projects"/>
+            <Redirect to="/projects" />
           </Route>
         </Switch>
       </main>
-
     </Router>
   );
 }
